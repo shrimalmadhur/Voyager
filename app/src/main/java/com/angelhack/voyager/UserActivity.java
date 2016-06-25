47b2ab51f03cf27c8aae9dd15b61bc4ac72e9eb7
Madhur Shrimal <madhur.shrimal@gmail.com>
2016-06-25 15:29:05 -0700

    Initial page commit

--- app/src/main/java/com/angelhack/voyager/UserActivity.java
+++ app/src/main/java/com/angelhack/voyager/UserActivity.java
@@ -2,6 +2,7 @@ package com.angelhack.voyager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+    }
+
+
+    // Create content Activity goes here
+    public void createTour(View view) {
+
+    }
+
+    // select a tour Activity goes here
+    public void selectTour(View view){
+
     }
 }
